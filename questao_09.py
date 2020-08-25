e81fd38949a86ebf05c256f8e15911964cd4ce32
liviosv15 <50671580+liviosv15@users.noreply.github.com>
2020-08-25 00:35:59 -0300

    Add files via upload

---
+++ questao_09.py
@@ -0,0 +1,3 @@
+fah = float(input('Informe a temperatura em graus Fahrenheit: '))
+cel = (fah-32)*5/9
+print('Atemperatura em celsius é: {}ºC'.format(cel))
---
+++ questao_10.py
@@ -0,0 +1,3 @@
+cel = float(input('Informe a temperatura em ºC: '))
+fah = (cel*9/5)+32
+print('A temperatura em Fahrenheit é: ', fah)
