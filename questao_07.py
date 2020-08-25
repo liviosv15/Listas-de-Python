07b7f4bfbe92dc013294a852bd1441936525b594
liviosv15 <50671580+liviosv15@users.noreply.github.com>
2020-08-25 00:14:58 -0300

    Add files via upload

---
+++ questao_07.py
@@ -0,0 +1,3 @@
+lado = int(input('Informe o lado do quadrado: '))
+area = lado**2
+print('O dobro da área é: ', area*2)
---
+++ questao_08.py
@@ -0,0 +1,5 @@
+money = float(input('Quanto vc ganha por hora? '))
+hora = float(input('Quantas horas vc trabalha por mês? '))
+salario = money*hora
+
+print("Seu salário este mês é: ", salario)
