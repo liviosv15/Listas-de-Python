f8fdb260767759e60652797cd6d5de682a7a0cc1
liviosv15 <50671580+liviosv15@users.noreply.github.com>
2020-08-25 02:23:58 -0300

    Add files via upload

---
+++ questao_11.py
@@ -0,0 +1,10 @@
+n1=int(input('Informe um número inteiro: '))
+n2=int(input('informe outro número inteiro: '))
+n3=float(input('Informe um número real: '))
+
+a = (n1*2)*(n2/2)
+print('O produto do dobro do primeiro com metade do segundo:', a)
+b = (n1*3)+n3
+print('a soma do triplo do primeiro com o terceiro:', b)
+c = n3*n3*n3
+print('o terceiro elevado ao cubo', c)
