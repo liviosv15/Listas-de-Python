749df3b7b6775855361ea6d5fe59a7fbd3a462de
liviosv15 <50671580+liviosv15@users.noreply.github.com>
2020-08-26 11:26:11 -0300

    Add files via upload

--- r_questao_14.py
+++ r_questao_14.py
@@ -3,15 +3,20 @@ nota2 = float(input('Informe a nota 2: '))
 media = (nota1+nota2)/2
 
 if (media>=9.0 and media<=10.0):
-    print('{} pontos, média de aproveitamento: A.'.format(media))
+    print('Suas notas foram: {} e {}, Sua média final: {}, média de aproveitamento: A.'.format(nota1, nota2, media))
+    print('Você foi APROVADO!')
 else:
     if (media>=7.5 and media<=9.0):
-        print('{} pontos, média de aproveitamento: B.'.format(media))
+        print('Suas notas foram: {} e {}, Sua média final: {}, média de aproveitamento: B.'.format(nota1, nota2, media))
+        print('Você foi APROVADO!')
     else:
         if (media>=6.0 and media<=7.5):
-            print('{} pontos, média de aproveitamento: C.'.format(media))
+            print('Suas notas foram: {} e {}, Sua média final: {}, média de aproveitamento: C.'.format(nota1, nota2, media))
+            print('Você foi APROVADO!')
         else:
             if (media>=4.0 and media<=6.0):
-                print('{} pontos, média de aproveitamento: D.'.format(media))
+                print('Suas notas foram: {} e {}, Sua média final: {}, média de aproveitamento: E.'.format(nota1, nota2, media))
+                print('Você foi REPROVADO!')
             else:
-                print('{} pontos, média de aproveitamento: E.'.format(media))
+                print('Suas notas foram: {} e {}, Sua média final: {}, média de aproveitamento: D.'.format(nota1, nota2, media))
+                print('Você foi REPROVADO!')
